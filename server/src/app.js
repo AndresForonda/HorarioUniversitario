a1234841c9deb584fda41e13a83c2168ebeb40b5
Your Name <andresforonda.af@gmail.com>
2018-07-27 21:33:47 +0000

    Configuracion del servidor express js

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+// Import modules to create the express application
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+// app is a variable to use express
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// this is a path or route => http://localhost:8081/status
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world'
+  })
+})
+
+// The server is listening on this port
+app.listen(process.env.PORT || 8081)
